10187764ef11787516375933c3c4f1b76599a13e
Corjaantje <choskam@gmail.com>
2016-05-17 15:44:58 +0200

    Corne (#53)
    * MainMenu Fix & Notifications
    
    Mainmenu no longer duplicates
    Notifications look better
    Better checking for conditions
    
    * Easier login
    
    Hit enter while in password to login
    
    * Fancyness!
    
    Mainmenu wallpaper
    
    * OCD fix
    
    Moved the wallpaper 2 pixels, OCD purpose!
    
    * Basic error removal
    
    * Chat updating switching game
    
    * Challenging User within Competition
    
    You can now challenge users to a game within a competition!
    
    * Language switching challenge
    
    Able to switch between english and dutch when challenging another player
    
    * Merge conflict Fix
    
    * Letter

--- src/GameObjects/Letter.java
+++ src/GameObjects/Letter.java
@@ -232,16 +232,6 @@ public class Letter implements Drawable {
 		return letter_id;
 	}
 	
-
-	public void setLetterID(int id){
-		letter_id = id;
-	}
-	
-	public int getLetterID(){
-		return letter_id;
-	}
-	
-
 	public int getScore()
 	{
 		return score;
