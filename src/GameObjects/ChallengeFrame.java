91004095c7e0789f7bdd81ffda819749158f103f
Corjaantje <choskam@gmail.com>
2016-05-17 14:41:33 +0200

    Language switching challenge
    Able to switch between english and dutch when challenging another player

--- src/GameObjects/ChallengeFrame.java
+++ src/GameObjects/ChallengeFrame.java
@@ -14,8 +14,8 @@ public class ChallengeFrame extends JFrame
 {
 	public ChallengeFrame(GamestateManager gsm, DatabaseController db_c)
 	{
-		this.setPreferredSize(new Dimension((int) (GUI.WIDTH/5), (int) (GUI.HEIGHT/4)));;
-		this.setLocation((int)(GUI.WIDTH/2.47), (int)(GUI.HEIGHT/6));
+		this.setPreferredSize(new Dimension((int) (GUI.WIDTH/4.9), (int) (GUI.HEIGHT/4)));;
+		this.setLocation((int)(GUI.WIDTH/2.49), (int)(GUI.HEIGHT/6));
 		this.setResizable(false);
 		this.setTitle("Speler Uitdagen");
 		this.setLayout(new BorderLayout());
