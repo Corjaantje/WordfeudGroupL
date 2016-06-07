427c21739866b36d1d2f1969ed3072cedc7f64e5
Marc Schinkel <mjschink@avans.nl>
2016-06-07 22:09:26 +0200

    Swap now working, tested for bugs too, i don't think there's any bugs in this now
    Swap now working, tested for bugs too, i don't think there's any bugs in
    this now

--- src/GameObjects/SwapFrame.java
+++ src/GameObjects/SwapFrame.java
@@ -5,6 +5,7 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 
 import Gamestate.GamestateManager;
+import Gamestate.Playstate;
 import Main.GUI;
 import controller.DatabaseController;
 
@@ -12,11 +13,11 @@ public class SwapFrame extends JFrame{
 	
 	private SwapPane swapPane;
 
-	public SwapFrame(LetterBox letterBox,DatabaseController db_c,GamestateManager gsm){
+	public SwapFrame(LetterBox letterBox,DatabaseController db_c,GamestateManager gsm, Playstate playState){
 		this.setLocation((int)(GUI.WIDTH/3), (int)GUI.HEIGHT/3);
 		this.setResizable(false);
 		this.setTitle("Verwissel je letters!");
-		swapPane = new SwapPane(letterBox,db_c,gsm);
+		swapPane = new SwapPane(letterBox,db_c,gsm, playState);
 		this.setContentPane(swapPane);
 		this.pack();
 	}
